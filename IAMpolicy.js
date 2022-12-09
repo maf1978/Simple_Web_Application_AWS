e07bba7183b84a410054c48eb1d0e74b0c2197a7
Marcos Ferrero <marcos.ferrero001@gmail.com>
2022-12-09 09:31:05 -0500

    Create IAMpolicy.js

---
+++ IAMpolicy.js
@@ -0,0 +1,18 @@
+{
+"Version": "2022-11-10",
+"Statement": [
+    {
+        "Sid": "VisualEditor0",
+        "Effect": "Allow",
+        "Action": [
+            "dynamodb:PutItem",
+            "dynamodb:DeleteItem",
+            "dynamodb:GetItem",
+            "dynamodb:Scan",
+            "dynamodb:Query",
+            "dynamodb:UpdateItem"
+        ],
+        "Resource": "YOUR-DB-TABLE-ARN"
+    }
+    ]
+}
